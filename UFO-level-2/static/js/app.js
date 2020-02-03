9c0ba52699889a916fefc3a3efa7f7f3513ab6c5
GoldenB <xinlianghuang85@gmail.com>
2020-02-03 16:35:39 -0600

    lv2 finished

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -24,44 +24,41 @@ tableData.forEach(function(ufoSighting){
 });
 // Select the button
 var button = d3.select("#filter-btn");
+var filteredData=[];
 button.on("click", function() {
-//date
-  // Select the input element and get the raw HTML node
-  var input_date = d3.select("#datetime");
-  // Get the value property of the input element
-  var enter_date= input_date.property("value");
-  console.log(enter_date);
-//city
-  // Select the input element and get the raw HTML node
-  var input_city = d3.select("#city");
-  // Get the value property of the input element
-  var enter_city= input_city.property("value");
-  console.log(enter_city);
-//state
-  // Select the input element and get the raw HTML node
-  var input_state = d3.select("#state");
-  // Get the value property of the input element
-  var enter_state= input_state.property("value");
-  console.log(enter_state);
-// country
-  // Select the input element and get the raw HTML node
-  var input_country = d3.select("#country");
-  // Get the value property of the input element
-  var enter_country= input_country.property("value");
-  console.log(enter_country);
-// shape
-  // Select the input element and get the raw HTML node
-  var input_shape = d3.select("#shape");
-  // Get the value property of the input element
-  var enter_shape= input_shape.property("value");
-  console.log(enter_shape) ;
-  var filteredData = tableData.filter(ufoSighting => (ufoSighting.datetime ===enter_date) ||
-                                                    (ufoSighting.city ===enter_city) ||
-                                                    (ufoSighting.state ===enter_state) ||
-                                                    (ufoSighting.country ===enter_country) ||
-                                                    (ufoSighting.shape ===enter_shape));
-  console.log(filteredData);
 
+  // Select the input element and get the raw HTML node
+  // Get the value property of the input element
+//   var enter_date= d3.select("#datetime").property("value");
+//   var enter_city= d3.select("#city").property("value");
+//   var enter_state= d3.select("#state").property("value");
+//   var enter_country= d3.select("#country").property("value");
+//   var enter_shape= d3.select("#shape").property("value");
+
+  //Set up conditions for each input element
+//   if (enter_date!==""){
+//     var filteredData = tableData.filter(ufoSighting => (ufoSighting.datetime ===enter_date) 
+//     )};
+//   if (enter_city!==""){
+//     var filteredData = tableData.filter(ufoSighting => (ufoSighting.city ===enter_city) 
+//     )};
+//   if (enter_state!==""){
+//     var filteredData = tableData.filter(ufoSighting => (ufoSighting.state ===enter_state) 
+//     )};
+//   if (enter_country!==""){
+//     var filteredData = tableData.filter(ufoSighting => (ufoSighting.country ===enter_country) 
+//     )};
+//   if (enter_shape!==""){
+//     var filteredData = tableData.filter(ufoSighting => (ufoSighting.shape ===enter_shape) 
+//     )};  
+var inputElement = d3.select("#input");
+var inputValue = inputElement.property("value");
+var filteredData = tableData.filter(ufoSighting => ufoSighting.datetime === inputValue ||
+    ufoSighting.city === inputValue ||
+    ufoSighting.state === inputValue ||
+    ufoSighting.country === inputValue ||
+    ufoSighting.shape === inputValue);
+console.log(filteredData);
 // Use D3 to select the table body
 var tbody = d3.select("tbody");
 // Use D3 to select the table
@@ -70,8 +67,8 @@ var table = d3.select("table");
 tbody.html("");
 // Use D3 to set the table class to `table table-striped`
 table.attr("class", "table table-striped");
-// Use d3 to append 1 cell per UFO sighting value 
-if (inputValue!=="") {filteredData.forEach(function(ufoSighting){
+
+filteredData.forEach(function(ufoSighting){
     console.log(ufoSighting);
     var row =tbody.append("tr");
 
@@ -80,17 +77,5 @@ if (inputValue!=="") {filteredData.forEach(function(ufoSighting){
         cell.text(value);
     });
 });
-}
-else {
-    tableData.forEach(function(ufoSighting){
-        console.log(ufoSighting);
-        var row =tbody.append("tr");
-    
-        Object.entries(ufoSighting).forEach(([key,value]) => {
-            var cell = row.append("td");
-            cell.text(value);
-        });
-    });
-}
 });
 
