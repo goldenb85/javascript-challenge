e02735ba56c667a9f51599574750cdaa073e051b
GoldenB <xinlianghuang85@gmail.com>
2020-05-25 15:56:35 -0500

    update ufo2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -6,6 +6,24 @@ var tableData = data;
 
 // Use D3 to select the table body
 var tbody = d3.select("tbody");
+function buildTable(data) {
+    // First, clear out any existing data
+    tbody.html("");
+  
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+      // Append a row to the table body
+      var row = tbody.append("tr");
+  
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
+        var cell = row.append("td");
+        cell.text(val);
+      });
+    });
+  }
 // Use D3 to select the table
 var table = d3.select("table");
 //remove any children from the table to 
