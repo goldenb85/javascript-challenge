afb7f5c122f8b99fac76f9c2ba0e76dfe17ea32a
GoldenB <xinlianghuang85@gmail.com>
2020-05-25 15:57:53 -0500

    comment previous code

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -24,78 +24,104 @@ function buildTable(data) {
       });
     });
   }
-// Use D3 to select the table
-var table = d3.select("table");
-//remove any children from the table to 
-tbody.html("");
-// Use D3 to set the table class to `table table-striped`
-table.attr("class", "table table-striped");
-// Use d3 to append 1 cell per UFO sighting value 
-tableData.forEach(function(ufoSighting){
-    console.log(ufoSighting);
-    var row =tbody.append("tr");
 
-    Object.entries(ufoSighting).forEach(([key,value]) => {
-        var cell = row.append("td");
-        cell.text(value);
-    });
-});
-// Select the button
-var button = d3.select("#filter-btn");
+
 // Keep Track of all filters
 var filters = {};
 
-button.on("click", function() {
+function updateFilters() {
 
-  // Select the input element and get the raw HTML node
-  // Get the value property of the input element
-//   var enter_date= d3.select("#datetime").property("value");
-//   var enter_city= d3.select("#city").property("value");
-//   var enter_state= d3.select("#state").property("value");
-//   var enter_country= d3.select("#country").property("value");
-//   var enter_shape= d3.select("#shape").property("value");
+  // Save the element, value, and id of the filter that was changed
+  var changedElement = d3.select(this).select("input");
+  var elementValue = changedElement.property("value");
+  var filterId = changedElement.attr("id");
 
-  //Set up conditions for each input element
-//   if (enter_date!==""){
-//     var filteredData = tableData.filter(ufoSighting => (ufoSighting.datetime ===enter_date) 
-//     )};
-//   if (enter_city!==""){
-//     var filteredData = tableData.filter(ufoSighting => (ufoSighting.city ===enter_city) 
-//     )};
-//   if (enter_state!==""){
-//     var filteredData = tableData.filter(ufoSighting => (ufoSighting.state ===enter_state) 
-//     )};
-//   if (enter_country!==""){
-//     var filteredData = tableData.filter(ufoSighting => (ufoSighting.country ===enter_country) 
-//     )};
-//   if (enter_shape!==""){
-//     var filteredData = tableData.filter(ufoSighting => (ufoSighting.shape ===enter_shape) 
-//     )};  
-var inputElement = d3.select("#input");
-var inputValue = inputElement.property("value");
-var filteredData = tableData.filter(ufoSighting => ufoSighting.datetime === inputValue ||
-    ufoSighting.city === inputValue ||
-    ufoSighting.state === inputValue ||
-    ufoSighting.country === inputValue ||
-    ufoSighting.shape === inputValue);
-console.log(filteredData);
-// Use D3 to select the table body
-var tbody = d3.select("tbody");
-// Use D3 to select the table
-var table = d3.select("table");
-//remove any children from the table to 
-tbody.html("");
-// Use D3 to set the table class to `table table-striped`
-table.attr("class", "table table-striped");
+  // If a filter value was entered then add that filterId and value
+  // to the filters list. Otherwise, clear that filter from the filters object
+  if (elementValue) {
+    filters[filterId] = elementValue;
+  }
+  else {
+    delete filters[filterId];
+  }
 
-filteredData.forEach(function(ufoSighting){
-    console.log(ufoSighting);
-    var row =tbody.append("tr");
+  // Call function to apply all filters and rebuild the table
+  filterTable();
 
-    Object.entries(ufoSighting).forEach(([key,value]) => {
-        var cell = row.append("td");
-        cell.text(value);
+}
+
+
+function filterTable() {
+
+    // Set the filteredData to the tableData
+    let filteredData = tableData;
+  
+    // Loop through all of the filters and keep any data that
+    // matches the filter values
+    Object.entries(filters).forEach(([key, value]) => {
+      filteredData = filteredData.filter(row => row[key] === value);
     });
-});
-});
+  
+    // Finally, rebuild the table using the filtered Data
+    buildTable(filteredData);
+  }
+  
+  // Attach an event to listen for changes to each filter
+  d3.selectAll(".filter").on("change", updateFilters);
+  
+  // Build the table when the page loads
+  buildTable(tableData);
+
+
+// // Use D3 to select the table
+// var table = d3.select("table");
+// //remove any children from the table to 
+// tbody.html("");
+// // Use D3 to set the table class to `table table-striped`
+// table.attr("class", "table table-striped");
+// // Use d3 to append 1 cell per UFO sighting value 
+// tableData.forEach(function(ufoSighting){
+//     console.log(ufoSighting);
+//     var row =tbody.append("tr");
+
+//     Object.entries(ufoSighting).forEach(([key,value]) => {
+//         var cell = row.append("td");
+//         cell.text(value);
+//     });
+// });
+// // Select the button
+// var button = d3.select("#filter-btn");
+// // Keep Track of all filters
+// var filters = {};
+
+// button.on("click", function() {
+
+ 
+// var inputElement = d3.select("#input");
+// var inputValue = inputElement.property("value");
+// var filteredData = tableData.filter(ufoSighting => ufoSighting.datetime === inputValue ||
+//     ufoSighting.city === inputValue ||
+//     ufoSighting.state === inputValue ||
+//     ufoSighting.country === inputValue ||
+//     ufoSighting.shape === inputValue);
+// console.log(filteredData);
+// // Use D3 to select the table body
+// var tbody = d3.select("tbody");
+// // Use D3 to select the table
+// var table = d3.select("table");
+// //remove any children from the table to 
+// tbody.html("");
+// // Use D3 to set the table class to `table table-striped`
+// table.attr("class", "table table-striped");
+
+// filteredData.forEach(function(ufoSighting){
+//     console.log(ufoSighting);
+//     var row =tbody.append("tr");
+
+//     Object.entries(ufoSighting).forEach(([key,value]) => {
+//         var cell = row.append("td");
+//         cell.text(value);
+//     });
+// });
+// });
 
